b20764324f7c80e204f8058470f79ed9c51be765
rahul <rahul352sj@gmail.com>
2024-12-04 17:53:08 +0530

    first

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -88,16 +88,17 @@ const Products = () => {
       image: '/products/a.jpg'
     },
   ])
+
   return (
     <Layout>
       <div className='md:w-8/12 w-9/12 m-auto py-8'>
         <h1 className='text-3xl font-bold'>All Products</h1>
         <p className='text-gray-600 mt-3 mb-3 text-sm md:text-base'>Bring home the latest products designed to blend sophistication with practicality. Whether for daily use or special occasions, this is the must-have item of the season.</p>
-        <div className='grid md:grid-cols-5 grid-cols-1 md:gap-8 gap-2 py-8'>
+        <div className='grid md:grid-cols-4 grid-cols-1 md:gap-8 gap-2 py-8'>
           {
             products.map((item, index) => (
               <div key={index} className='m-auto border pb-2 rounded-lg shadow-xl'>
-              <img src={item.image} alt="" className='md:h-[280px] md:w-[210px] object-cover rounded-lg' />
+              <img src={item.image} alt="" className='object-cover rounded-lg' />
               <div className='flex flex-col items-start justify-start gap-y-1 mt-2 p-2'>
                   <h1 className='text-gray-600 text-base capitalize'>{item.title}</h1>
                   <div className=' '>
@@ -105,7 +106,7 @@ const Products = () => {
                     <del className='text-red-600'>₹{item.price}</del>
                     <label className='text-green-600'>({item.discount}% off)</label>
                   </div>
-                  <button 
+                  <button
                     className='mt-1 rounded-lg bg-green-600 py-1 w-full px-3 text-white hover:bg-green-700' 
                     style={{
                      transition:'0.3s'
